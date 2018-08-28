417d108aeb4275efdd9ecab8dec8ec9a0c7b3693
Asbjørn Thorsen <reglund.thorsen@gmail.com>
2018-08-28 21:21:13 +0200

    starten på gloseprogrammet

---
+++ gloser.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+$gloser = { "fish" => "fisk", "spoon" => "skje", "window" => "vindu", 
+           "music" => "musikk", "cat" => "katt"}
+
+
+def init
+  puts "Dette er et gloseprogram. Skriv inn utenlandsk til norsk (A) eller motsatt (B)"
+  valg = gets.chomp
+  case valg
+  when "A"
+    puts "du valgte A"
+  when "B"
+    puts "du valgte B"
+  else
+    die("Du valgte et ulovlig valg")
+  end
+end
+
+def les_fil
+  File.open("gloser.txt").each do |line|
+    puts line
+  end
+end
+
+def pugg_gloser
+  $gloser.each do |utenl,norsk|
+    print ""
+  end
+end
+
+init()
+pugg_gloser()
+#les_fil()
